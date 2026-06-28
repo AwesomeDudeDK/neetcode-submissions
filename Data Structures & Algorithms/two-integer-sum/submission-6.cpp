f466f9976eaaf4876fd036e91848d788f24451ba
Daniel Jeyeon Kim <86535763+AwesomeDudeDK@users.noreply.github.com>
2026-06-28 11:46:14 +0900

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        std::unordered_map<int, int> m;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int diff = target - nums[i];
+            if (m.find(diff) != m.end()) {
+                return {m[diff], i};
+            }
+            m.insert({nums[i], i});
+        }
+        
+        return {};
+    }
+};
